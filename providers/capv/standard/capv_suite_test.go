58eee34062b7d4c4cdc4c45afb7fb58195ec7311
Jirka Kremser <535866+jkremser@users.noreply.github.com>
2023-05-30 15:28:50 +0200

    Add CAPV provider (#16)
    * Add CAPV provider
    
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>
    
    * Update providers/capv/standard/capv_suite_test.go
    
    Co-authored-by: Marcus Noble <AverageMarcus@users.noreply.github.com>
    Signed-off-by: Jirka Kremser <535866+jkremser@users.noreply.github.com>
    
    * Update providers/capv/standard/capv_suite_test.go
    
    Co-authored-by: Marcus Noble <AverageMarcus@users.noreply.github.com>
    Signed-off-by: Jirka Kremser <535866+jkremser@users.noreply.github.com>
    
    * Update cluster values & don't depend on fixed versions
    
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>
    
    * Add extraConfig for vsphere cluster app to handle docker.io pull limits
    
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>
    
    ---------
    
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>
    Signed-off-by: Jirka Kremser <535866+jkremser@users.noreply.github.com>
    Co-authored-by: Marcus Noble <AverageMarcus@users.noreply.github.com>

---
+++ providers/capv/standard/capv_suite_test.go
@@ -0,0 +1,98 @@
+package standard
+
+import (
+	"context"
+	"path"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	cr "sigs.k8s.io/controller-runtime/pkg/client"
+
+	applicationv1alpha1 "github.com/giantswarm/apiextensions-application/api/v1alpha1"
+	"github.com/giantswarm/cluster-test-suites/common"
+	"github.com/giantswarm/clustertest"
+	"github.com/giantswarm/clustertest/pkg/application"
+	"github.com/giantswarm/clustertest/pkg/logger"
+	"github.com/giantswarm/clustertest/pkg/organization"
+	"github.com/giantswarm/clustertest/pkg/utils"
+	"github.com/giantswarm/clustertest/pkg/wait"
+)
+
+const (
+	KubeContext       = "capv"
+	RegcredSecretName = "container-registries-configuration"
+	defaultNamespace  = "giantswarm"
+)
+
+var (
+	ctx       context.Context
+	framework *clustertest.Framework
+	cluster   *application.Cluster
+)
+
+func TestCAPVStandard(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CAPV Standard Suite")
+}
+
+var _ = BeforeSuite(func() {
+	ctx = context.Background()
+	logger.LogWriter = GinkgoWriter
+
+	var err error
+	framework, err = clustertest.New(KubeContext)
+	Expect(err).NotTo(HaveOccurred())
+
+	cluster = setUpWorkloadCluster()
+
+	common.Framework = framework
+	common.Cluster = cluster
+})
+
+func setUpWorkloadCluster() *application.Cluster {
+	cluster, err := framework.LoadCluster()
+	Expect(err).NotTo(HaveOccurred())
+	if cluster != nil {
+		logger.Log("Using existing cluster %s/%s", cluster.Name, cluster.Namespace)
+		return cluster
+	}
+
+	return createCluster()
+}
+
+func createCluster() *application.Cluster {
+	cluster = application.NewClusterApp(utils.GenerateRandomName("t"), application.ProviderVSphere).
+		WithOrg(organization.New(defaultNamespace)). // Uses the `giantswarm` org (and namespace) as it requires a credentials secret to exist already
+		WithAppValuesFile(path.Clean("./test_data/cluster_values.yaml"), path.Clean("./test_data/default-apps_values.yaml")).
+		WithUserConfigSecret("vsphere-credentials").
+		WithExtraConfigs([]applicationv1alpha1.AppExtraConfig{
+			{
+				Kind:      "secret",
+				Name:      RegcredSecretName,
+				Namespace: defaultNamespace,
+				Priority:  25,
+			},
+		})
+
+	logger.Log("Workload cluster name: %s", cluster.Name)
+
+	applyCtx, cancelApplyCtx := context.WithTimeout(ctx, 20*time.Minute)
+	defer cancelApplyCtx()
+
+	client, err := framework.ApplyCluster(applyCtx, cluster)
+	Expect(err).NotTo(HaveOccurred())
+
+	Eventually(
+		wait.AreNumNodesReady(ctx, client, 1, &cr.MatchingLabels{"node-role.kubernetes.io/control-plane": ""}),
+		20*time.Minute, 15*time.Second,
+	).Should(BeTrue())
+
+	DeferCleanup(func() {
+		Expect(framework.DeleteCluster(ctx, cluster)).To(Succeed())
+	})
+
+	return cluster
+}
