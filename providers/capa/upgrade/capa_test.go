01b3b7ef0ee25cb0d7a822a7c906e6846e77df11
calvix <rozsypalek.vaclav@gmail.com>
2023-12-13 14:20:28 +0100

    capa-disable-bastions (#164)

--- providers/capa/upgrade/capa_test.go
+++ providers/capa/upgrade/capa_test.go
@@ -13,7 +13,7 @@ var _ = Describe("Basic upgrade test", Ordered, func() {
 	// Finally run the common tests after upgrade is completed
 	common.Run(&common.TestConfig{
 		AutoScalingSupported: true,
-		BastionSupported:     true,
+		BastionSupported:     false,
 		ExternalDnsSupported: true,
 	})
 })
