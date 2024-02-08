0952bde722dbee1c763d7383de8cfdcd9d6ae975
Marie Roque <marie@giantswarm.io>
2024-02-08 14:03:31 +0100

    Add tests on DeploymentSets, StatefulSets and DaemonSets to ensure all desired replicas/pods are running

--- internal/common/basic.go
+++ internal/common/basic.go
@@ -10,6 +10,7 @@ import (
 	"github.com/giantswarm/clustertest/pkg/client"
 	"github.com/giantswarm/clustertest/pkg/logger"
 	"github.com/giantswarm/clustertest/pkg/wait"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	cr "sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -72,6 +73,27 @@ func runBasic() {
 
 		It("has all of its Pods in the Running state", func() {
 			Eventually(wait.Consistent(checkAllPodsSuccessfulPhase(wcClient), 10, time.Second)).
+				WithTimeout(15 * time.Minute).
+				WithPolling(wait.DefaultInterval).
+				Should(Succeed())
+		})
+
+		It("has all its Deployments Ready (means all replicas are running)", func() {
+			Eventually(wait.Consistent(checkAllDeploymentsReady(wcClient), 10, time.Second)).
+				WithTimeout(15 * time.Minute).
+				WithPolling(wait.DefaultInterval).
+				Should(Succeed())
+		})
+
+		It("has all its StatefulSets Ready (means all replicas are running)", func() {
+			Eventually(wait.Consistent(checkAllStatefulSetsReady(wcClient), 10, time.Second)).
+				WithTimeout(15 * time.Minute).
+				WithPolling(wait.DefaultInterval).
+				Should(Succeed())
+		})
+
+		It("has all its DaemonSets Ready (means all daemon pods are running)", func() {
+			Eventually(wait.Consistent(checkAllDaemonSetsReady(wcClient), 10, time.Second)).
 				WithTimeout(15 * time.Minute).
 				WithPolling(wait.DefaultInterval).
 				Should(Succeed())
@@ -141,6 +163,72 @@ func checkAllPodsSuccessfulPhase(wcClient *client.Client) func() error {
 		}
 
 		logger.Log("All (%d) pods currently in a running or completed state", len(podList.Items))
+		return nil
+	}
+}
+
+func checkAllDeploymentsReady(wcClient *client.Client) func() error {
+	return func() error {
+		deploymentList := &appsv1.DeploymentList{}
+		err := wcClient.List(context.Background(), deploymentList)
+		if err != nil {
+			return err
+		}
+
+		for _, deployment := range deploymentList.Items {
+			available := deployment.Status.AvailableReplicas
+			desired := *deployment.Spec.Replicas
+			if available != desired {
+				logger.Log("deployment %s/%s has %d/%d replicas available", deployment.Namespace, deployment.Name, available, desired)
+				return fmt.Errorf("deployment %s/%s has %d/%d replicas available", deployment.Namespace, deployment.Name, available, desired)
+			}
+		}
+
+		logger.Log("All (%d) deployments have all replicas running", len(deploymentList.Items))
+		return nil
+	}
+}
+
+func checkAllStatefulSetsReady(wcClient *client.Client) func() error {
+	return func() error {
+		statefulSetList := &appsv1.StatefulSetList{}
+		err := wcClient.List(context.Background(), statefulSetList)
+		if err != nil {
+			return err
+		}
+
+		for _, statefulSet := range statefulSetList.Items {
+			available := statefulSet.Status.AvailableReplicas
+			desired := *statefulSet.Spec.Replicas
+			if available != desired {
+				logger.Log("statefulSet %s/%s has %d/%d replicas available", statefulSet.Namespace, statefulSet.Name, available, desired)
+				return fmt.Errorf("statefulSet %s/%s has %d/%d replicas available", statefulSet.Namespace, statefulSet.Name, available, desired)
+			}
+		}
+
+		logger.Log("All (%d) statefulSets have all replicas running", len(statefulSetList.Items))
+		return nil
+	}
+}
+
+func checkAllDaemonSetsReady(wcClient *client.Client) func() error {
+	return func() error {
+		daemonSetList := &appsv1.DaemonSetList{}
+		err := wcClient.List(context.Background(), daemonSetList)
+		if err != nil {
+			return err
+		}
+
+		for _, daemonSet := range daemonSetList.Items {
+			current := daemonSet.Status.CurrentNumberScheduled
+			desired := daemonSet.Status.DesiredNumberScheduled
+			if current != desired {
+				logger.Log("daemonSet %s/%s has %d/%d daemon pods available", daemonSet.Namespace, daemonSet.Name, current, desired)
+				return fmt.Errorf("daemonSet %s/%s has %d/%d daemon pods available", daemonSet.Namespace, daemonSet.Name, current, desired)
+			}
+		}
+
+		logger.Log("All (%d) daemonSets have all daemon pods running", len(daemonSetList.Items))
 		return nil
 	}
 }
