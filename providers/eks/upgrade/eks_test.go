0df488d31d0862593b77ac3c35a47cd0a88e2345
Marcus Noble <AverageMarcus@users.noreply.github.com>
2024-03-14 11:41:59 +0000

    Disable EKS tests (#242)
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- providers/eks/upgrade/eks_test.go
+++ providers/eks/upgrade/eks_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/giantswarm/cluster-test-suites/internal/upgrade"
 )
 
-var _ = Describe("Basic upgrade test", Ordered, func() {
+var _ = XDescribe("Basic upgrade test", Ordered, func() {
 	upgrade.Run(upgrade.NewTestConfigWithDefaults())
 
 	// Finally run the common tests after upgrade is completed
