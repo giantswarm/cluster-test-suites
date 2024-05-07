0dae544ac98bb8cd2132015463c254fb31918b64
Erkan Erol <erkan@giantswarm.io>
2024-05-07 19:00:25 +0300

    Add CAPV on CAPZ tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+
+### Added
+
+- CAPV on CAPZ tests.
+
 ### Changed
 
 - Make error messages actionable for Prometheus metrics query test failures
---
+++ providers/capv/on-capz/capv_suite_test.go
@@ -0,0 +1,19 @@
+package standard
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/giantswarm/cluster-standup-teardown/pkg/clusterbuilder/providers/capv"
+
+	"github.com/giantswarm/cluster-test-suites/internal/suite"
+)
+
+func TestCAPVStandard(t *testing.T) {
+	suite.Setup(false, &capv.ClusterBuilder{"capv-on-capz"})
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CAPV on CAPZ Suite")
+}
