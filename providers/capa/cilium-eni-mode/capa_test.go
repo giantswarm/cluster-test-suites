d878dc69f3690e89f945bf476369e32f5fd70637
Andreas Sommer <andreas@giantswarm.io>
2024-05-07 15:35:30 +0200

    Check that Cilium ENI mode pod IPs are assigned from secondary CIDR

--- providers/capa/cilium-eni-mode/capa_test.go
+++ providers/capa/cilium-eni-mode/capa_test.go
@@ -1,12 +1,19 @@
 package cilium_eni_mode
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 
 	"github.com/giantswarm/cluster-test-suites/internal/common"
+	"github.com/giantswarm/cluster-test-suites/internal/state"
 )
 
-var _ = Describe("Common tests", func() {
+var _ = Describe("Cilium ENI mode tests", Ordered, func() {
 	common.Run(&common.TestConfig{
 		AutoScalingSupported:         true,
 		BastionSupported:             false,
@@ -14,4 +21,33 @@ var _ = Describe("Common tests", func() {
 		ExternalDnsSupported:         true,
 		ControlPlaneMetricsSupported: true,
 	})
+
+	runSecondaryPodIPs()
 })
+
+func runSecondaryPodIPs() {
+	It("assigns IP addresses from secondary VPC CIDR to pods", func() {
+		wcClient, err := state.GetFramework().WC(state.GetCluster().Name)
+		if err != nil {
+			Fail(err.Error())
+		}
+
+		podList := &corev1.PodList{}
+		err = wcClient.List(context.Background(), podList)
+		if err != nil {
+			Fail(err.Error())
+		}
+
+		numPodsWithSecondaryCIDRPodIP := 0
+		foundPodIPs := []string{}
+		for _, pod := range podList.Items {
+			if strings.HasPrefix(pod.Status.PodIP, "10.1.") {
+				numPodsWithSecondaryCIDRPodIP++
+			}
+
+			foundPodIPs = append(foundPodIPs, pod.Status.PodIP)
+		}
+
+		Expect(numPodsWithSecondaryCIDRPodIP).Should(BeNumerically(">=", 5), fmt.Sprintf("Many pods (except those on host network, for example) should have an IP assigned from the secondary CIDR block 10.1.0.0/16. Found these pod IPs: %s", strings.Join(foundPodIPs, ", ")))
+	})
+}
