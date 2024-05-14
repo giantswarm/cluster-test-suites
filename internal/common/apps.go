9482efd8ac18f854392f479e6369adb3cf2b2f35
Nikola Prokopić <nikola@prokopic.rs>
2024-05-14 12:46:59 +0200

    Fix var name

--- internal/common/apps.go
+++ internal/common/apps.go
@@ -19,13 +19,13 @@ import (
 func runApps() {
 	Context("default apps", func() {
 		It("all default apps are deployed without issues", func() {
-			skipDefaultApps, err := state.GetCluster().UsesUnifiedClusterApp()
+			skipDefaultAppsApp, err := state.GetCluster().UsesUnifiedClusterApp()
 			Expect(err).NotTo(HaveOccurred())
 
 			// We need to wait for default-apps to be deployed before we can check all apps.
 			defaultAppsAppName := fmt.Sprintf("%s-%s", state.GetCluster().Name, "default-apps")
 
-			if skipDefaultApps {
+			if skipDefaultAppsApp {
 				logger.Log("Checking default apps deployed from the unified %s app (with default apps), so skipping check of %s App resource as it does not exist.", state.GetCluster().ClusterApp.AppName, state.GetCluster().DefaultAppsApp.AppName)
 			} else {
 				Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), defaultAppsAppName, state.GetCluster().Organization.GetNamespace())).
@@ -35,7 +35,7 @@ func runApps() {
 			}
 
 			var defaultAppsSelectorLabels ctrl.MatchingLabels
-			if skipDefaultApps {
+			if skipDefaultAppsApp {
 				defaultAppsSelectorLabels = ctrl.MatchingLabels{
 					"giantswarm.io/cluster":        state.GetCluster().Name,
 					"app.kubernetes.io/managed-by": "Helm",
