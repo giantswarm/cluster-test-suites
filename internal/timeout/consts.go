90af186e65caa1b817e0770f76970d8bc13772ce
Marcus Noble <AverageMarcus@users.noreply.github.com>
2024-07-15 07:55:03 +0100

    Added framework for overriding test timeouts (#396)
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

---
+++ internal/timeout/consts.go
@@ -0,0 +1,6 @@
+package timeout
+
+const (
+	// DeployApps is used by "all default apps are deployed without issues"
+	DeployApps TestKey = "deployAppsTimeout"
+)
--- providers/capa/china/capa_test.go
+++ providers/capa/china/capa_test.go
@@ -1,18 +1,19 @@
 package china
 
 import (
-	"context"
-	. "github.com/onsi/ginkgo/v2"
 	"time"
+
+	. "github.com/onsi/ginkgo/v2"
 
 	"github.com/giantswarm/cluster-test-suites/internal/common"
 	"github.com/giantswarm/cluster-test-suites/internal/state"
+	"github.com/giantswarm/cluster-test-suites/internal/timeout"
 )
 
 var _ = Describe("Common tests", func() {
 	BeforeEach(func() {
 		// Set the timeout for deploying apps to 25 minutes for China test
-		state.SetContext(context.WithValue(state.GetContext(), common.DeployAppsTimeoutContextKey, time.Minute*25))
+		state.SetTestTimeout(timeout.DeployApps, time.Minute*25)
 	})
 
 	common.Run(&common.TestConfig{
