2e10d7eb08eaac2ee54ab5d3d42409312a2b068e
Antonia <56017655+anvddriesch@users.noreply.github.com>
2024-08-19 18:04:04 +0900

    add capv on capa tests (#419)
    * add capv on capa tests
    
    * disable proxy
    
    ---------
    
    Signed-off-by: Antonia <56017655+anvddriesch@users.noreply.github.com>

---
+++ providers/capv/on-capa/capv_test.go
@@ -0,0 +1,19 @@
+package standard
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+
+	"github.com/giantswarm/cluster-test-suites/internal/common"
+)
+
+var _ = Describe("Common tests", func() {
+	common.Run(&common.TestConfig{
+		// No autoscaling on-prem
+		AutoScalingSupported: false,
+		BastionSupported:     false,
+		TeleportSupported:    true,
+		// Disabled until https://github.com/giantswarm/roadmap/issues/1037
+		ExternalDnsSupported:         false,
+		ControlPlaneMetricsSupported: true,
+	})
+})
