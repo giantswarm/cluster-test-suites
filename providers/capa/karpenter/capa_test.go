950abdbad19beed633117ac42a06bdfb80ffa9d3
Jose Armesto <github@armesto.net>
2025-06-23 16:53:36 +0200

    Increase timeout because karpenter workers take longer to come up

--- providers/capa/karpenter/capa_test.go
+++ providers/capa/karpenter/capa_test.go
@@ -1,12 +1,21 @@
 package standard
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo/v2" //nolint:staticcheck
 
 	"github.com/giantswarm/cluster-test-suites/internal/common"
+	"github.com/giantswarm/cluster-test-suites/internal/state"
+	"github.com/giantswarm/cluster-test-suites/internal/timeout"
 )
 
 var _ = Describe("Common tests", func() {
+	BeforeEach(func() {
+		// Set the timeout for deploying apps to 25 minutes for karpenter test, because workers take longer to come up
+		state.SetTestTimeout(timeout.DeployApps, time.Minute*25)
+	})
+
 	common.Run(&common.TestConfig{
 		AutoScalingSupported:         true,
 		BastionSupported:             false,
