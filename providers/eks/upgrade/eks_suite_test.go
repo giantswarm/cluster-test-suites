b5aa789d0b67cde24ea35181912fab85d1f3ca85
Jose Armesto <github@armesto.net>
2025-09-15 02:34:18 -0700

    Don't rely on workers having labels (#784)

--- providers/eks/upgrade/eks_suite_test.go
+++ providers/eks/upgrade/eks_suite_test.go
@@ -8,18 +8,17 @@ import (
 	. "github.com/onsi/gomega"    //nolint:staticcheck
 
 	"github.com/giantswarm/cluster-standup-teardown/pkg/clusterbuilder/providers/capa"
-	"github.com/giantswarm/clustertest/pkg/client"
+	clustertestclient "github.com/giantswarm/clustertest/pkg/client"
 	"github.com/giantswarm/clustertest/pkg/wait"
-	cr "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/giantswarm/cluster-test-suites/internal/state"
 	"github.com/giantswarm/cluster-test-suites/internal/suite"
 )
 
 func TestEKSUpgrade(t *testing.T) {
-	suite.Setup(true, &capa.ManagedClusterBuilder{}, func(client *client.Client) {
+	suite.Setup(true, &capa.ManagedClusterBuilder{}, func(client *clustertestclient.Client) {
 		Eventually(
-			wait.AreNumNodesReady(state.GetContext(), client, 2, &cr.MatchingLabels{"node-role.kubernetes.io/worker": ""}),
+			wait.AreNumNodesReady(state.GetContext(), client, 2, clustertestclient.DoesNotHaveLabels{"node-role.kubernetes.io/control-plane"}),
 			20*time.Minute, 15*time.Second,
 		).Should(BeTrue())
 	})
